4bafd5bb3e209df91b6a3a0c7726395094434d0a
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2024-11-22 16:27:28 +0200

    Fix ISO C violations

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -6,7 +6,7 @@
 #include "limine.h"
 
 __attribute__((used, section(".limine_requests")))
-static volatile LIMINE_BASE_REVISION(3);
+static volatile LIMINE_BASE_REVISION(3)
 
 __attribute__((used, section(".limine_requests")))
 static volatile struct limine_framebuffer_request fb_request = {
@@ -15,13 +15,13 @@ static volatile struct limine_framebuffer_request fb_request = {
 };
 
 __attribute__((used, section(".limine_requests_start")))
-static volatile LIMINE_REQUESTS_START_MARKER;
+static volatile LIMINE_REQUESTS_START_MARKER
 
 __attribute__((used, section(".limine_requests_end")))
-static volatile LIMINE_REQUESTS_END_MARKER;
+static volatile LIMINE_REQUESTS_END_MARKER
 
 static void halt(void) {
-    asm volatile("hlt");
+    __asm__ volatile("cli; hlt");
 }
 
 void kmain(void) {
