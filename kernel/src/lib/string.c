ce6106e37fbe180529324ff0536200767add9b6e
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2024-12-08 16:33:57 +0200

    Provide `strcmp` and `strncmp` implementations

---
+++ kernel/src/lib/string.c
@@ -0,0 +1,30 @@
+#include "lib/string.h"
+
+int strcmp(const char *s1, const char *s2) {
+    const unsigned char *p1 = (const unsigned char *) s1;
+    const unsigned char *p2 = (const unsigned char *) s2;
+
+    while (*p1 && *p1 == *p2) {
+        ++p1;
+        ++p2;
+    }
+
+    return (*p1 > *p2) - (*p2 > *p1);
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+    const unsigned char *p1 = (const unsigned char *) s1;
+    const unsigned char *p2 = (const unsigned char *) s2;
+
+    while (n && *p1 && (*p1 == *p2)) {
+        ++p1;
+        ++p2;
+        --n;
+    }
+
+    if (n == 0) {
+        return 0;
+    } else {
+        return (*p1 - *p2);
+    }
+}
---
+++ kernel/src/lib/string.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stddef.h>
+
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
