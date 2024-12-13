5e764723fcd06b22062b2e4a1b717b509f625fb4
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2024-12-13 15:48:59 +0200

    Improve PMM portability

--- kernel/src/memory/pmm/pmm.c
+++ kernel/src/memory/pmm/pmm.c
@@ -6,12 +6,12 @@
 #include "memory/vmm/vmm.h"
 
 static struct bitmap_t bitmap;
-static uint64_t page_allocation_start;
+static phys_t page_allocation_start;
 
 void pmm_init(struct limine_memmap_response *memmap) {
     struct limine_memmap_entry *largest_usable_entry = memmap->entries[0];
 
-    for (size_t i = 0; i < memmap->entry_count; i++) {
+    for (uint64_t i = 0; i < memmap->entry_count; i++) {
         struct limine_memmap_entry *entry = memmap->entries[i];
 
         if (entry->type == LIMINE_MEMMAP_USABLE && entry->length > largest_usable_entry->length) {
@@ -30,7 +30,8 @@ void pmm_init(struct limine_memmap_response *memmap) {
     uint64_t pages_used_to_store_bitmap = div_and_align_up(usable_pages, 8 * PAGE_SIZE);
 
     // start allocating physical memory after the bitmap
-    page_allocation_start = largest_usable_entry->base + pages_used_to_store_bitmap * PAGE_SIZE;
+    page_allocation_start
+        = (phys_t) (largest_usable_entry->base + pages_used_to_store_bitmap * PAGE_SIZE);
 
     bitmap.start = (uint8_t *) (largest_usable_entry->base + vmm_get_hhdm_offset());
     bitmap.bit_count = usable_pages - pages_used_to_store_bitmap;
@@ -41,7 +42,7 @@ void pmm_init(struct limine_memmap_response *memmap) {
     kprintf("PMM initialized with %dMiB of avl. phys. mem.\n", largest_usable_entry->length >> 20);
 }
 
-void *pmm_alloc(bool zero_contents) {
+phys_t pmm_alloc(bool zero_contents) {
     uint64_t page_index = 0;
     while (page_index < bitmap.bit_count && bitmap_get_bit(&bitmap, page_index)) {
         page_index++;
@@ -52,31 +53,32 @@ void *pmm_alloc(bool zero_contents) {
     } else {
         bitmap_set_bit(&bitmap, page_index);
 
-        uint64_t page_phys = page_allocation_start + page_index * PAGE_SIZE;
+        phys_t page_addr = page_allocation_start + page_index * PAGE_SIZE;
+
         if (!zero_contents) {
-            return (void *) page_phys;
+            return page_addr;
         }
 
-        uint8_t *page_start = (uint8_t *) (page_phys + vmm_get_hhdm_offset());
+        uint8_t *page_start = (uint8_t *) (page_addr + vmm_get_hhdm_offset());
         uint8_t *page_end = (uint8_t *) (page_start + PAGE_SIZE);
         for (uint8_t *i = page_start; i < page_end; i++) {
             *i = 0;
         }
 
-        return (void *) page_phys;
+        return page_addr;
     }
 }
 
-void *pmm_alloc_n(uint64_t n_pages, bool zero_contents) {
-    uint8_t found_n_pages = 0;
+phys_t pmm_alloc_n(uint64_t n_pages, bool zero_contents) {
+    bool found_n_pages = false;
     uint64_t page_index = 0;
     while (page_index < bitmap.bit_count - n_pages + 1) {
         if (!bitmap_get_bit(&bitmap, page_index)) {
-            found_n_pages = 1;
+            found_n_pages = true;
             
             for (uint64_t i = page_index; i < page_index + n_pages; i++) {
                 if (bitmap_get_bit(&bitmap, i)) {
-                    found_n_pages = 0;
+                    found_n_pages = false;
                     break;
                 }
             }
@@ -96,27 +98,29 @@ void *pmm_alloc_n(uint64_t n_pages, bool zero_contents) {
             bitmap_set_bit(&bitmap, i);
         }
 
-        uintptr_t page_phys = page_allocation_start + page_index * PAGE_SIZE;
+        phys_t page_addr = page_allocation_start + page_index * PAGE_SIZE;
+
         if (!zero_contents) {
-            return (void *) page_phys;
+            return page_addr;
         }
 
-        uint8_t *page_start = (uint8_t *) (page_phys + vmm_get_hhdm_offset());
+        uint8_t *page_start = (uint8_t *) (page_addr + vmm_get_hhdm_offset());
         uint8_t *page_end = (uint8_t *) (page_start + n_pages * PAGE_SIZE);
         for (uint8_t *i = page_start; i < page_end; i++) {
             *i = 0;
         }
-        return (void *) page_phys;
+
+        return page_addr;
     }
 }
 
-void pmm_free(void *addr) {
-    uint64_t page_index = ((uint64_t) addr - page_allocation_start) / PAGE_SIZE;
+void pmm_free(phys_t addr) {
+    uint64_t page_index = (addr - page_allocation_start) / PAGE_SIZE;
     bitmap_unset_bit(&bitmap, page_index);
 }
 
-void pmm_free_n(void *addr, uint64_t n_pages) {
-    uint64_t page_index = ((uint64_t) addr - page_allocation_start) / PAGE_SIZE;
+void pmm_free_n(phys_t addr, uint64_t n_pages) {
+    uint64_t page_index = (addr - page_allocation_start) / PAGE_SIZE;
     for (uint64_t i = page_index; i < page_index + n_pages; i++) {
         bitmap_unset_bit(&bitmap, i);
     }
@@ -146,10 +150,10 @@ static char *get_entry_type(uint64_t entry_type) {
 }
 
 void pmm_print_memmap(struct limine_memmap_response *memmap) {
-    for (size_t i = 0; i < memmap->entry_count; i++) {
+    for (uint64_t i = 0; i < memmap->entry_count; i++) {
         struct limine_memmap_entry *entry = memmap->entries[i];
-        uint64_t start = entry->base;
-        uint64_t end = entry->base + entry->length;
+        phys_t start = entry->base;
+        phys_t end = entry->base + entry->length;
         uint64_t length_in_mib = entry->length >> 20;
         char *type = get_entry_type(entry->type);
         kprintf(" * 0x%016llx - 0x%016llx | %dMiB - %s\n", start, end, length_in_mib, type);
--- kernel/src/memory/pmm/pmm.h
+++ kernel/src/memory/pmm/pmm.h
@@ -7,9 +7,11 @@
 
 #define PAGE_SIZE 4096
 
+typedef uint64_t phys_t;
+
 void pmm_init(struct limine_memmap_response *memmap);
-void *pmm_alloc(bool zero_contents);
-void *pmm_alloc_n(uint64_t n_pages, bool zero_contents);
-void pmm_free(void *addr);
-void pmm_free_n(void *addr, uint64_t n_pages);
+phys_t pmm_alloc(bool zero_contents);
+phys_t pmm_alloc_n(uint64_t n_pages, bool zero_contents);
+void pmm_free(phys_t addr);
+void pmm_free_n(phys_t addr, uint64_t n_pages);
 void pmm_print_memmap(struct limine_memmap_response *memmap);
