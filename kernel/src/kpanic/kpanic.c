c0451c99e949ea70c60d1dcbae80906d8d705470
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2025-01-15 20:11:46 +0200

    Wrap common kernel panic code for reusing

--- kernel/src/kpanic/kpanic.c
+++ kernel/src/kpanic/kpanic.c
@@ -8,6 +8,22 @@
 
 static char kpanic_buf[256];
 
+static void print_int_ctx(struct int_ctx_t *ctx) {
+    klog_fatal("vector 0x%02x ec %x", ctx->vector, ctx->error_code);
+    klog_fatal("rax %016llx    rbx %016llx", ctx->rax, ctx->rbx);
+    klog_fatal("rcx %016llx    rdx %016llx", ctx->rcx, ctx->rdx);
+    klog_fatal("rdi %016llx    rsi %016llx", ctx->rdi, ctx->rsi);
+    klog_fatal("rbp %016llx    rsp %016llx", ctx->rbp, ctx->rsp);
+    klog_fatal("r8  %016llx    r9  %016llx", ctx->r8, ctx->r9);
+    klog_fatal("r10 %016llx    r11 %016llx", ctx->r10, ctx->r11);
+    klog_fatal("r12 %016llx    r13 %016llx", ctx->r12, ctx->r13);
+    klog_fatal("r14 %016llx    r15 %016llx", ctx->r14, ctx->r15);
+    klog_fatal("cr0 %016llx    cr2 %016llx", ctx->cr0, ctx->cr2);
+    klog_fatal("cr3 %016llx    cr4 %016llx", ctx->cr3, ctx->cr4);
+    klog_fatal("rip %016llx rflags %016llx", ctx->rip, ctx->rflags);
+    klog_fatal("cs %04x ss %04x", ctx->cs, ctx->ss);
+}
+
 static void do_stacktrace(void) {
     uint64_t rbp;
     __asm__ volatile("mov %%rbp, %0" : "=m" (rbp));
@@ -25,33 +41,23 @@ static void do_stacktrace(void) {
 }
 
 __attribute__((noreturn))
+static inline void kvpanic(struct int_ctx_t *ctx, const char *reason, va_list va) {
+    vsnprintf_(kpanic_buf, sizeof(kpanic_buf), reason, va);
+    klog_fatal("KERNEL PANIC on CPU #%llu >>> %s", get_cpu()->id, kpanic_buf);
+    do_stacktrace();
+    if (ctx != NULL) print_int_ctx(ctx);
+    mp_halt_all_cpus();
+    while (1) halt();
+}
+
+__attribute__((noreturn))
 void kpanic(const char *reason, ...) {
     cpu_set_int_state(false);
 
     va_list va;
     va_start(va, reason);
-    vsnprintf_(kpanic_buf, sizeof(kpanic_buf), reason, va);
+    kvpanic(NULL, reason, va);
     va_end(va);
-
-    klog_fatal("KERNEL PANIC on CPU #%llu >>> %s", get_cpu()->id, kpanic_buf);
-    do_stacktrace();
-    mp_halt_all_cpus();
-}
-
-static void print_int_ctx(struct int_ctx_t *ctx) {
-    klog_fatal("vector 0x%02x ec %x", ctx->vector, ctx->error_code);
-    klog_fatal("rax %016llx    rbx %016llx", ctx->rax, ctx->rbx);
-    klog_fatal("rcx %016llx    rdx %016llx", ctx->rcx, ctx->rdx);
-    klog_fatal("rdi %016llx    rsi %016llx", ctx->rdi, ctx->rsi);
-    klog_fatal("rbp %016llx    rsp %016llx", ctx->rbp, ctx->rsp);
-    klog_fatal("r8  %016llx    r9  %016llx", ctx->r8, ctx->r9);
-    klog_fatal("r10 %016llx    r11 %016llx", ctx->r10, ctx->r11);
-    klog_fatal("r12 %016llx    r13 %016llx", ctx->r12, ctx->r13);
-    klog_fatal("r14 %016llx    r15 %016llx", ctx->r14, ctx->r15);
-    klog_fatal("cr0 %016llx    cr2 %016llx", ctx->cr0, ctx->cr2);
-    klog_fatal("cr3 %016llx    cr4 %016llx", ctx->cr3, ctx->cr4);
-    klog_fatal("rip %016llx rflags %016llx", ctx->rip, ctx->rflags);
-    klog_fatal("cs %04x ss %04x", ctx->cs, ctx->ss);
 }
 
 __attribute__((noreturn))
@@ -60,11 +66,6 @@ void kpanic_int_ctx(struct int_ctx_t *ctx, const char *reason, ...) {
 
     va_list va;
     va_start(va, reason);
-    vsnprintf_(kpanic_buf, sizeof(kpanic_buf), reason, va);
+    kvpanic(ctx, reason, va);
     va_end(va);
-
-    klog_fatal("KERNEL PANIC on CPU #%llu >>> %s", get_cpu()->id, kpanic_buf);
-    print_int_ctx(ctx);
-    do_stacktrace();
-    mp_halt_all_cpus();
 }
