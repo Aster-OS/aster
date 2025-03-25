bd5836e0f0e662fafd4631f9409df08af2b6a8c4
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2025-03-25 21:12:54 +0200

    Add `strlen` to `lib/strutil`

--- kernel/src/lib/strutil.c
+++ kernel/src/lib/strutil.c
@@ -12,6 +12,20 @@ int strcmp(const char *s1, const char *s2) {
     return (*p1 > *p2) - (*p2 > *p1);
 }
 
+size_t strlen(const char *s) {
+    if (!s) {
+        return 0;
+    }
+
+    const unsigned char *str = (const unsigned char *) s;
+    const unsigned char *ptr = (const unsigned char *) s;
+    while (*ptr) {
+        ptr++;
+    }
+
+    return ptr - str;
+}
+
 int strncmp(const char *s1, const char *s2, size_t n) {
     const unsigned char *p1 = (const unsigned char *) s1;
     const unsigned char *p2 = (const unsigned char *) s2;
--- kernel/src/lib/strutil.h
+++ kernel/src/lib/strutil.h
@@ -3,4 +3,5 @@
 #include <stddef.h>
 
 int strcmp(const char *s1, const char *s2);
+size_t strlen(const char *s);
 int strncmp(const char *s1, const char *s2, size_t n);
