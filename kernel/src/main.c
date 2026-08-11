d133560be039380d49645c9e9a25473fd858fa93
Vlad-Robert Baciu <vladrobertbaciu@gmail.com>
2026-08-11 13:13:05 +0300

    Drop printing commit hash inside kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -146,7 +146,6 @@ void kernel_entry(void) {
 
     klog_info("Aster booted by %s v%s", bootloader_info->name,
               bootloader_info->version);
-    klog_info("Built at commit \"%s\"", COMMIT_HASH);
 
     char cpu_brand_str[48];
     if (cpu_get_brand_str(cpu_brand_str)) {
